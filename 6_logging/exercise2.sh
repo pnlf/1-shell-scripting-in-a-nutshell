43fda866609b0d10c962e955ec22189b5248d2eb
Pedro Nuno Fernandes <pnlfernandes@gmail.com>
2020-12-27 20:00:44 +0100

    Solved exercises 2 of chapter 6

---
+++ 6_logging/exercise2.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Modify the previous script so that it uses a logging function. 
+# Additionally tag each syslog message with "randomly" and include the process ID. 
+# Generate 3 random numbers.
